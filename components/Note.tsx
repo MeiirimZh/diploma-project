d5a3a67b342f09997b406995400a64afa04077b3
MeiirimZh <zh.meyirim@gmail.com>
2026-01-19 15:41:46 +0500

    added Note component

---
+++ components/Note.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet, View } from "react-native";
+
+export default function Note() {
+    return (
+        <View>
+            <View style={ styles.content } >
+                
+            </View>
+
+            <View style={ styles.desc } >
+
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    content: {
+
+    },
+    desc: {
+
+    }
+});
